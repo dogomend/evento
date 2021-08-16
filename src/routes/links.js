626aba7e4980b808128c806ef5b18dc7c1d33526
chehin <chehinjuan@gmail.com>
2021-08-15 21:00:04 -0300

    Vistas comparte y detalle

--- src/routes/links.js
+++ src/routes/links.js
@@ -9,7 +9,7 @@ router.get('/add', isLoggedIn, (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', isLoggedIn, async (req, res) => {
+router.post('/add', isLoggedIn, async(req, res) => {
     const { title, url, description, lugar, precio, fecha, destacado } = req.body;
     const newLink = {
         title,
@@ -25,48 +25,67 @@ router.post('/add', isLoggedIn, async (req, res) => {
     req.flash('success', 'Evento Saved Successfully');
     res.redirect('/links');
 });
-router.get('/total', async (req,  res) => {
-//console.log('kjuuhgfdsj');
-   const event = await pool.query('SELECT * FROM event');
-  res.render('links/total', { event });
+router.get('/total', async(req, res) => {
+    //console.log('kjuuhgfdsj');
+    const event = await pool.query('SELECT * FROM event');
+    res.render('links/total', { event });
 });
 
-router.get('/detalle/:id', async (req,  res) => {
-//console.log('kjuuhgfdsj');
-const { id } = req.params;
-   const event = await pool.query('SELECT * FROM event WHERE user_id = ?', [id]);
-  res.render('links/detalle', { event });
+router.get('/detalle/:id', async(req, res) => {
+    //console.log('kjuuhgfdsj');
+    const { id } = req.params;
+    const event = await pool.query('SELECT * FROM event WHERE user_id = ?', [id]);
+    console.log('event es : ', event);
+    console.log('event[0] es : ', event[0].title);
+    console.log('event.title es : ', event.title);
+
+
+    res.render('links/detalle', {
+        event: {
+            title: event[0].title,
+            description: event[0].description,
+            lugar: event[0].lugar,
+            url: event[0].url,
+            precio: event[0].precio,
+            fecha: event[0].fecha,
+            created_at: event[0].created_at
+        }
+    });
 });
 
-router.get('/comparte/:id', async (req,  res) => {
-const { id } = req.params;
-   const event = await pool.query('SELECT * FROM event WHERE user_id = ?', [id]);
-  res.render('links/comparte', { event });
+router.get('/comparte/:id', async(req, res) => {
+    const { id } = req.params;
+    const event = await pool.query('SELECT * FROM event WHERE user_id = ?', [id]);
+    res.render('links/comparte', {
+        event: {
+            title: event[0].title
+        }
+    });
 });
 
-router.get('/', isLoggedIn, async (req, res) => {
-  
+router.get('/', isLoggedIn, async(req, res) => {
+
     const event = await pool.query('SELECT * FROM event WHERE user_id = ?', [req.user.id]);
     res.render('links/list', { event });
 });
 
 
-router.get('/delete/:id', isLoggedIn, async (req, res) => {
+router.get('/delete/:id', isLoggedIn, async(req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM event WHERE ID = ?', [id]);
     req.flash('success', 'Evento Removed Successfully');
     res.redirect('/links');
 });
 
-router.get('/edit/:id', isLoggedIn, async (req, res) => {
+router.get('/edit/:id', isLoggedIn, async(req, res) => {
     const { id } = req.params;
     const event = await pool.query('SELECT * FROM event WHERE id = ?', [id]);
-    res.render('links/edit', {event: event[0]});
+    res.render('links/edit', { event: event[0] });
 });
 
-router.post('/edit/:id', isLoggedIn, async (req, res) => {
+router.post('/edit/:id', isLoggedIn, async(req, res) => {
     const { id } = req.params;
-    const { title, url, description, lugar, precio, fecha, destacado} = req.body; 
+    const { title, url, description, lugar, precio, fecha, destacado } = req.body;
     const newLink = {
         title,
         url,
